75848b7dd86bceab723b1ea5fbbaacf62f3b57a7
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2019-07-31 12:57:48 -0300

    Mudança do absolute url

--- SimpleMooc/courses/models.py
+++ SimpleMooc/courses/models.py
@@ -21,6 +21,9 @@ class Course(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return ('courses:details', (), {'slug': self.slug})
+
     class Meta:
         verbose_name = 'Curso'
         verbose_name_plural = 'Cursos'
